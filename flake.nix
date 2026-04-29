0b9262ad540a277c536a715281142d7e39af6411
trev <me@trev.xyz>
2026-04-29 03:26:07 -0400

    bump: v0.1.0 -> v0.1.1

--- flake.nix
+++ flake.nix
@@ -88,7 +88,7 @@
           default = pkgs.stdenv.mkDerivation (
             final: with pkgs.lib; {
               pname = "zig-template";
-              version = "0.1.0";
+              version = "0.1.1";
 
               src = fileset.toSource {
                 root = ./.;
